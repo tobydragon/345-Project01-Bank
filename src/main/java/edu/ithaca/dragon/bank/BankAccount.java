2d0c57c99665eb38d3bac5a70d79ae1c18ee0bc3
Toby Dragon <dr.toby.dragon@gmail.com>
2021-02-15 09:33:11 -0500

    fixed withdraw again

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -30,7 +30,7 @@ public class BankAccount {
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
-        if (amount <= balance || amount >= 0){
+        if (amount <= balance || amount > 0){
             balance -= amount;
         }
         else {
